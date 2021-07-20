29ca998cafed086738aca2d35730a6e033d156d8
ranupardhi <87702740+ranupardhi@users.noreply.github.com>
2021-07-20 12:33:36 -0230

    Update Details.php

--- Details.php
+++ Details.php
@@ -171,7 +171,7 @@
 
     <nav id="sideNav">
       <ul>
-        <li><a href="Index.html">HOME</a></li>
+        <li><a href="index.html">HOME</a></li>
         <li><a href="users.php">All CUSTOMERS</a></li>
         <li><a href="history.php">TRANSACTION HISTORY</a></li>
         <li><a href="users.php">TRANSFER MONEY</a></li>
